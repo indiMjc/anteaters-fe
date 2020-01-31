42fe3452530fd1249654df4e9e688a374176d97f
indiMjc <45151346+indiMjc@users.noreply.github.com>
2020-01-30 20:30:59 -0500

    started making the dropdown menu for tablet and mobile views

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Hamburger from './components/Hamburger';
 
 const App = props => {
+	const [menuClass, setMenuClass] = useState('closed');
+
+	const navClick = () => {
+		const css = menuClass === 'closed' ? 'opened' : 'closed';
+		setMenuClass(css);
+	};
+
 	return (
 		<div className='App'>
-			<Navbar {...props} />
+			<Navbar {...props} navClass={menuClass} />
+			<Hamburger onClick={navClick} />
 		</div>
 	);
 };
